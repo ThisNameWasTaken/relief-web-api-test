807eb545826b28d7d3c675ddccd4d2daa1e6e90b
Adrian <adriandinca2008@yahoo.com>
2018-10-21 08:12:40 +0300

    feat: Get all disasters stats

--- main.js
+++ main.js
@@ -2,20 +2,47 @@ import fetchPost from './fetchPost.js';
 
 (async function () {
     const data = await fetchPost('https://api.reliefweb.int/v1/reports?appname=apidoc', {
-        "limit": 30,
+        "limit": 20,
+        // "profile": "list",
+        "profile": "full",
+        // "preset": "analysis",
         "query": {
-            "value": "earthquake",
-            "fields": ["disaster"]
+            "value": "disaster"
         },
-        "filter": {
-            "operator": "OR",
-            "conditions": [
-                {
-                    "field": "disaster",
-                    "status": "alert"
-                }
-            ]
+        // "fields": {
+        //     "include": [
+        //         "primary_country",
+        //         "status",
+        //         "theme",
+        //         "url",
+        //         "origin",
+        //         "body-html",
+        //         "body",
+        //         "file",
+        //         "disaster_type"
+        //     ]
+        // },
+        // "verbose": 1,
+        // "fields": {
+        //     "include": ["disaster.name"]
+        // },
+        // related_glide
+        "filter":
+        {
+            "operator": "AND",
+            "conditions": [{
+                "field": "primary_country",
+                "value": "France"
+            }, {
+                "field": "language.name",
+                "value": "english"
+            }]
         }
+    });
+
+    data.data.forEach(article => {
+        // console.log();
+        document.body.innerHTML += (article.fields['body-html'] + '<hr/>');
     });
 
     console.log(data);
